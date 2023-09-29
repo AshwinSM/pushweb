d4af6cad62faf0bebd9af837ad905dbc602102c9
AshwinSM <ashwinmalaiyandi@gmail.com>
2023-09-30 01:43:42 +0400

    Changing the Client Ids

--- push.js
+++ push.js
@@ -7,12 +7,21 @@ let vapid =  {
 
 push.setVapidDetails('mailto:test@code.co.uk',vapid.publicKey,vapid.privateKey);
 
-let sub = {
-    endpoint:"https://fcm.googleapis.com/fcm/send/eXN-PrLyO9s:APA91bGJCb1_aVFMUwFVe5xV8WAfxZhBuuCrF8HsmJXYvyIOJXIuoBhkc9ks2fAwZbhMVS2inPxOrAk3PT3T2v1BkypW1lMti_CbZ1prBgAsHRe3IrJAj1iGnLQzAD_aUZ6qkrlq0P4C",
+// let sub = {
+//     endpoint:"https://fcm.googleapis.com/fcm/send/eXN-PrLyO9s:APA91bGJCb1_aVFMUwFVe5xV8WAfxZhBuuCrF8HsmJXYvyIOJXIuoBhkc9ks2fAwZbhMVS2inPxOrAk3PT3T2v1BkypW1lMti_CbZ1prBgAsHRe3IrJAj1iGnLQzAD_aUZ6qkrlq0P4C",
+//     expirationTime:null,
+//     keys:{
+//         p256dh:"BItxqTvyoPh0M-SM2IfU--AwZJFT_lYfIxzY5IDUIABAm4g0mcj1ADE43sdIuVXTnswXUrkDJJC8dxPWOF7feK0",
+//         auth:"Fr9Iaso_0_rWcn70mIWH9w"
+//     }
+// }
+
+let sub =  {
+    endpoint:"https://fcm.googleapis.com/fcm/send/csxCQBtmWv4:APA91bEJxVBvjAmYWK0ZXPHevMU1npZmVdX6CVybz01WMVE3Nhn8wcKFLLMg_7ghmCQvIlBiOPCBLl9_O2H5f4O8xM_ulZZR7TKuKaHlh99-TJ-ZcvohmwRgY9vBBHRLm6Q8FxqR9Qq_",
     expirationTime:null,
     keys:{
-        p256dh:"BItxqTvyoPh0M-SM2IfU--AwZJFT_lYfIxzY5IDUIABAm4g0mcj1ADE43sdIuVXTnswXUrkDJJC8dxPWOF7feK0",
-        auth:"Fr9Iaso_0_rWcn70mIWH9w"
+        p256dh:"BOPbJptS4pmG4J_bIM4x5q6B6T6c05z5PFNfHELgl6zHK0w4NicjAr_q7VxLNKHI2PPHKjQ0Slllcjs1VayWKWE",
+        auth:"ETGDdwi_LJAcjoOL9c8cQQ"
     }
 }
 
--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@ self.addEventListener('push', function(event) {
     console.log('[Service Worker] Push Received.');
     console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
   
-    const title = 'Push Codelab';
+    const title = 'CAT360 elite';
     const options = {
-      body: 'Yay it works.',
+      body: 'There is a notification for you from CAT360',
       icon: 'images/icon.png',
       badge: 'images/badge.png'
     };
