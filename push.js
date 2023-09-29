32f634c438723b46d9a1b124711df02ca85f43d8
AshwinSM <ashwinmalaiyandi@gmail.com>
2023-09-30 01:08:00 +0400

    Service Worker Event Listener Fix

--- push.js
+++ push.js
@@ -5,8 +5,15 @@ let vapid =  {
     "privateKey":"8PANSQGtsQ-CQTQADDqEwhxRugs5yiPHhGob9hdZmzY"
 };
 
-push.setVapidDetails('Test Message',vapid.publicKey,vapid.privateKey);
+push.setVapidDetails('mailto:test@code.co.uk',vapid.publicKey,vapid.privateKey);
 
-let sub = {};
+let sub = {
+    endpoint:"https://fcm.googleapis.com/fcm/send/eXN-PrLyO9s:APA91bGJCb1_aVFMUwFVe5xV8WAfxZhBuuCrF8HsmJXYvyIOJXIuoBhkc9ks2fAwZbhMVS2inPxOrAk3PT3T2v1BkypW1lMti_CbZ1prBgAsHRe3IrJAj1iGnLQzAD_aUZ6qkrlq0P4C",
+    expirationTime:null,
+    keys:{
+        p256dh:"BItxqTvyoPh0M-SM2IfU--AwZJFT_lYfIxzY5IDUIABAm4g0mcj1ADE43sdIuVXTnswXUrkDJJC8dxPWOF7feK0",
+        auth:"Fr9Iaso_0_rWcn70mIWH9w"
+    }
+}
 
 push.sendNotification(sub, 'Test Notification Message');
